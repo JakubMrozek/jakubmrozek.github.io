31c0da5e00545c88c57e4a1465e8fea07d8181d1
Jakub Mrozek <jakub.mrozek@gmail.com>
2019-02-01 06:06:58 +0900

    chinese

---
+++ chinese-source.js
@@ -0,0 +1,57 @@
+const text = 
+` 
+你好	nǐhǎo	Hello
+爸爸	bàba	Father
+儿子	érzi	Son
+再见	zàijiàn	Goodbye
+是	shì	To be
+我	wǒ	I
+你	nǐ	You
+他,她	tā	He, she
+妈妈	māma	Mother
+女儿	nǚ'ér	Daughter
+中国	zhōngguó	China
+美国	měiguó	USA
+人	rén	person，people
+不	bù	not
+们	men	plural
+爱	ài	love
+猫	māo	cat
+狗	gǒu	dog
+都	dōu	both or all
+哥哥	gēge	older brother
+姐姐	jiějie	older sister
+有	yǒu	to have
+弟弟	dìdi	younger brother
+妹妹	mèimei	younger sister
+没有	méiyǒu	not to have
+的	de	of
+熊猫	xióngmāo	panda
+这	zhè	this
+猪	zhū	pig
+那	nà	that
+也	yě	too
+牛	niú	cow
+羊	yáng	sheep
+吗	ma	question y/n
+鱼	yú	fish
+鸡	jī	chicken
+鸟	niǎo	bird
+猪肉	zhūròu	pork (vepřové)
+牛肉	niúròu	beef (hovězí)
+吃	chī	to eat
+面包	miànbāo	bread
+蔬菜	shūcài	vegetables
+和	hé	and
+茶	chá	tea
+咖啡	kāfēi	coffee
+喝	hē	to drink
+酒	jiǔ	alcohol
+果汁	guǒzhì	juice
+可乐	kělè	cola
+喜欢	xǐhuān	to like
+喜	xǐ	to like, be found of
+欢	huān	happy, joyous
+什么	shénme	what
+叫	jiào	to be called
+`
